4386f2247f0f1005ffd86c11c46af643bf79c662
Manik Sharma <casanova270407@gmail.com>
2025-06-12 18:08:45 +0530

    completed task

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -2,11 +2,13 @@ import Chai from './chai'
 import './App.css'
 
 function App() {
-  
+  const username = "Manik sharma"/* jo usernamwe humne likha h woh ek evaluated expression 
+  h js ka ek eval;uated expression */
   return (
-    
-        <Chai/>
-     
+    <>
+      <Chai />
+      <h1>Manik sharma plays valo {username}</h1>
+    </>
   )
 }
 
--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyApp()
+{
+  return (
+    <div>
+      <h1>CUSTOM App !!</h1>
+    </div>
+  )
+}
+// const reactElement={
+//     type:'a',
+//     props:{
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'click me to visit google'
+// }
+
+const anotherElement=(<a href='https://youtube.com' target='_blank'>visit youtube</a>)
+
+const anotherUser="filter cofffeWE"
+const reactElement=React.createElement(
+  'a',
+ { href:'https://google.com',target:'_blank'},
+  'click me magr aaram se',
+  anotherUser
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ //<App/>
+ reactElement
 )
