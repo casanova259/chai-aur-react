d64f2a9df2150f91475c4f0e9d9a202c198cf84c
Manik Sharma <casanova270407@gmail.com>
2025-06-12 16:55:45 +0530

    Added vitereact project

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './chai'
+import './App.css'
+
+function App() {
+  
+  return (
+    
+        <Chai/>
+     
+  )
+}
+
+export default App
---
+++ vitereact/src/chai.jsx
@@ -0,0 +1,5 @@
+function Chai()
+{
+     return(<h1>MANIK SHARMA</h1>) 
+}
+export default Chai;
